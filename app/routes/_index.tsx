6bd152974e3c6aaed6068d8aaa0870b9f90f2c02
Devvrat <devvratshukla@gmail.com>
2024-11-06 17:50:05 +0000

    chore: repo init

---
+++ app/routes/_index.tsx
@@ -0,0 +1,20 @@
+import { json, type MetaFunction } from '@remix-run/cloudflare';
+import { ClientOnly } from 'remix-utils/client-only';
+import { BaseChat } from '~/components/chat/BaseChat';
+import { Chat } from '~/components/chat/Chat.client';
+import { Header } from '~/components/header/Header';
+
+export const meta: MetaFunction = () => {
+  return [{ title: 'Slingshot' }, { name: 'description', content: 'AI Code Assistant' }];
+};
+
+export const loader = () => json({});
+
+export default function Index() {
+  return (
+    <div className="flex flex-col h-full w-full">
+      <Header />
+      <ClientOnly fallback={<BaseChat model="" setModel={() => {}} />}>{() => <Chat />}</ClientOnly>
+    </div>
+  );
+}
